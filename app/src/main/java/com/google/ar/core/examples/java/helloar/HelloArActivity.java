23afaa703bbb8241db257dc718c4a29d4a20e2b3
Otto Seiskari <otto.seiskari@gmail.com>
2019-08-03 13:56:23 +0300

    Augmented Faces MVP: Andy on your nose

--- app/src/main/java/com/google/ar/core/examples/java/helloar/HelloArActivity.java
+++ app/src/main/java/com/google/ar/core/examples/java/helloar/HelloArActivity.java
@@ -21,30 +21,20 @@ import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.widget.Toast;
-import com.google.ar.core.Anchor;
+
 import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.AugmentedFace;
 import com.google.ar.core.Camera;
+import com.google.ar.core.Config;
 import com.google.ar.core.Frame;
-import com.google.ar.core.HitResult;
-import com.google.ar.core.Plane;
-import com.google.ar.core.Point;
-import com.google.ar.core.Point.OrientationMode;
-import com.google.ar.core.PointCloud;
 import com.google.ar.core.Session;
-import com.google.ar.core.Trackable;
-import com.google.ar.core.TrackingState;
 import com.google.ar.core.examples.java.common.helpers.CameraPermissionHelper;
 import com.google.ar.core.examples.java.common.helpers.DisplayRotationHelper;
 import com.google.ar.core.examples.java.common.helpers.FullScreenHelper;
 import com.google.ar.core.examples.java.common.helpers.SnackbarHelper;
-import com.google.ar.core.examples.java.common.helpers.TapHelper;
 import com.google.ar.core.examples.java.common.rendering.BackgroundRenderer;
 import com.google.ar.core.examples.java.common.rendering.ObjectRenderer;
-import com.google.ar.core.examples.java.common.rendering.ObjectRenderer.BlendMode;
-import com.google.ar.core.examples.java.common.rendering.PlaneRenderer;
-import com.google.ar.core.examples.java.common.rendering.PointCloudRenderer;
 import com.google.ar.core.exceptions.CameraNotAvailableException;
 import com.google.ar.core.exceptions.UnavailableApkTooOldException;
 import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
@@ -52,7 +42,8 @@ import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
 import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
 import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.EnumSet;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
@@ -72,32 +63,12 @@ public class HelloArActivity extends AppCompatActivity implements GLSurfaceView.
   private Session session;
   private final SnackbarHelper messageSnackbarHelper = new SnackbarHelper();
   private DisplayRotationHelper displayRotationHelper;
-  private TapHelper tapHelper;
 
   private final BackgroundRenderer backgroundRenderer = new BackgroundRenderer();
   private final ObjectRenderer virtualObject = new ObjectRenderer();
-  private final ObjectRenderer virtualObjectShadow = new ObjectRenderer();
-  private final PlaneRenderer planeRenderer = new PlaneRenderer();
-  private final PointCloudRenderer pointCloudRenderer = new PointCloudRenderer();
 
   // Temporary matrix allocated here to reduce number of allocations for each frame.
   private final float[] anchorMatrix = new float[16];
-  private static final float[] DEFAULT_COLOR = new float[] {0f, 0f, 0f, 0f};
-
-  private static final String SEARCHING_PLANE_MESSAGE = "Searching for surfaces...";
-
-  // Anchors created from taps used for object placing with a given color.
-  private static class ColoredAnchor {
-    public final Anchor anchor;
-    public final float[] color;
-
-    public ColoredAnchor(Anchor a, float[] color4f) {
-      this.anchor = a;
-      this.color = color4f;
-    }
-  }
-
-  private final ArrayList<ColoredAnchor> anchors = new ArrayList<>();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -105,10 +76,6 @@ public class HelloArActivity extends AppCompatActivity implements GLSurfaceView.
     setContentView(R.layout.activity_main);
     surfaceView = findViewById(R.id.surfaceview);
     displayRotationHelper = new DisplayRotationHelper(/*context=*/ this);
-
-    // Set up tap listener.
-    tapHelper = new TapHelper(/*context=*/ this);
-    surfaceView.setOnTouchListener(tapHelper);
 
     // Set up renderer.
     surfaceView.setPreserveEGLContextOnPause(true);
@@ -145,7 +112,10 @@ public class HelloArActivity extends AppCompatActivity implements GLSurfaceView.
         }
 
         // Create the session.
-        session = new Session(/* context= */ this);
+        session = new Session(/* context= */ this, EnumSet.of(Session.Feature.FRONT_CAMERA));
+        Config config = new Config(session);
+        config.setAugmentedFaceMode(Config.AugmentedFaceMode.MESH3D);
+        session.configure(config);
 
       } catch (UnavailableArcoreNotInstalledException
           | UnavailableUserDeclinedInstallationException e) {
@@ -228,16 +198,9 @@ public class HelloArActivity extends AppCompatActivity implements GLSurfaceView.
     try {
       // Create the texture and pass it to ARCore session to be filled during update().
       backgroundRenderer.createOnGlThread(/*context=*/ this);
-      planeRenderer.createOnGlThread(/*context=*/ this, "models/trigrid.png");
-      pointCloudRenderer.createOnGlThread(/*context=*/ this);
 
       virtualObject.createOnGlThread(/*context=*/ this, "models/andy.obj", "models/andy.png");
       virtualObject.setMaterialProperties(0.0f, 2.0f, 0.5f, 6.0f);
-
-      virtualObjectShadow.createOnGlThread(
-          /*context=*/ this, "models/andy_shadow.obj", "models/andy_shadow.png");
-      virtualObjectShadow.setBlendMode(BlendMode.Shadow);
-      virtualObjectShadow.setMaterialProperties(1.0f, 0.0f, 0.0f, 1.0f);
 
     } catch (IOException e) {
       Log.e(TAG, "Failed to read an asset file", e);
@@ -271,18 +234,8 @@ public class HelloArActivity extends AppCompatActivity implements GLSurfaceView.
       Frame frame = session.update();
       Camera camera = frame.getCamera();
 
-      // Handle one tap per frame.
-      handleTap(frame, camera);
-
       // If frame is ready, render camera preview image to the GL surface.
       backgroundRenderer.draw(frame);
-
-      // If not tracking, don't draw 3D objects, show tracking failure reason instead.
-      if (camera.getTrackingState() == TrackingState.PAUSED) {
-        messageSnackbarHelper.showMessage(
-            this, TrackingStateHelper.getTrackingFailureReasonString(camera));
-        return;
-      }
 
       // Get projection matrix.
       float[] projmtx = new float[16];
@@ -298,99 +251,21 @@ public class HelloArActivity extends AppCompatActivity implements GLSurfaceView.
       final float[] colorCorrectionRgba = new float[4];
       frame.getLightEstimate().getColorCorrection(colorCorrectionRgba, 0);
 
-      // Visualize tracked points.
-      // Use try-with-resources to automatically release the point cloud.
-      try (PointCloud pointCloud = frame.acquirePointCloud()) {
-        pointCloudRenderer.update(pointCloud);
-        pointCloudRenderer.draw(viewmtx, projmtx);
-      }
+      for (AugmentedFace face : session.getAllTrackables(AugmentedFace.class)) {
+        float scaleFactor = 0.25f;
+        float[] color4f = { 1f, 1f, 1f, 1f };
 
-      // No tracking error at this point. If we detected any plane, then hide the
-      // message UI, otherwise show searchingPlane message.
-      if (hasTrackingPlane()) {
-        messageSnackbarHelper.hide(this);
-      } else {
-        messageSnackbarHelper.showMessage(this, SEARCHING_PLANE_MESSAGE);
-      }
-
-      // Visualize planes.
-      planeRenderer.drawPlanes(
-          session.getAllTrackables(Plane.class), camera.getDisplayOrientedPose(), projmtx);
-
-      // Visualize anchors created by touch.
-      float scaleFactor = 1.0f;
-      for (ColoredAnchor coloredAnchor : anchors) {
-        if (coloredAnchor.anchor.getTrackingState() != TrackingState.TRACKING) {
-          continue;
-        }
-        // Get the current pose of an Anchor in world space. The Anchor pose is updated
-        // during calls to session.update() as ARCore refines its estimate of the world.
-        coloredAnchor.anchor.getPose().toMatrix(anchorMatrix, 0);
+        face.getRegionPose(AugmentedFace.RegionType.NOSE_TIP)
+                .toMatrix(anchorMatrix, 0);
 
         // Update and draw the model and its shadow.
         virtualObject.updateModelMatrix(anchorMatrix, scaleFactor);
-        virtualObjectShadow.updateModelMatrix(anchorMatrix, scaleFactor);
-        virtualObject.draw(viewmtx, projmtx, colorCorrectionRgba, coloredAnchor.color);
-        virtualObjectShadow.draw(viewmtx, projmtx, colorCorrectionRgba, coloredAnchor.color);
+        virtualObject.draw(viewmtx, projmtx, colorCorrectionRgba, color4f);
       }
 
     } catch (Throwable t) {
       // Avoid crashing the application due to unhandled exceptions.
       Log.e(TAG, "Exception on the OpenGL thread", t);
     }
-  }
-
-  // Handle only one tap per frame, as taps are usually low frequency compared to frame rate.
-  private void handleTap(Frame frame, Camera camera) {
-    MotionEvent tap = tapHelper.poll();
-    if (tap != null && camera.getTrackingState() == TrackingState.TRACKING) {
-      for (HitResult hit : frame.hitTest(tap)) {
-        // Check if any plane was hit, and if it was hit inside the plane polygon
-        Trackable trackable = hit.getTrackable();
-        // Creates an anchor if a plane or an oriented point was hit.
-        if ((trackable instanceof Plane
-                && ((Plane) trackable).isPoseInPolygon(hit.getHitPose())
-                && (PlaneRenderer.calculateDistanceToPlane(hit.getHitPose(), camera.getPose()) > 0))
-            || (trackable instanceof Point
-                && ((Point) trackable).getOrientationMode()
-                    == OrientationMode.ESTIMATED_SURFACE_NORMAL)) {
-          // Hits are sorted by depth. Consider only closest hit on a plane or oriented point.
-          // Cap the number of objects created. This avoids overloading both the
-          // rendering system and ARCore.
-          if (anchors.size() >= 20) {
-            anchors.get(0).anchor.detach();
-            anchors.remove(0);
-          }
-
-          // Assign a color to the object for rendering based on the trackable type
-          // this anchor attached to. For AR_TRACKABLE_POINT, it's blue color, and
-          // for AR_TRACKABLE_PLANE, it's green color.
-          float[] objColor;
-          if (trackable instanceof Point) {
-            objColor = new float[] {66.0f, 133.0f, 244.0f, 255.0f};
-          } else if (trackable instanceof Plane) {
-            objColor = new float[] {139.0f, 195.0f, 74.0f, 255.0f};
-          } else {
-            objColor = DEFAULT_COLOR;
-          }
-
-          // Adding an Anchor tells ARCore that it should track this position in
-          // space. This anchor is created on the Plane to place the 3D model
-          // in the correct position relative both to the world and to the plane.
-          anchors.add(new ColoredAnchor(hit.createAnchor(), objColor));
-          break;
-        }
-      }
-    }
-  }
-
-  /** Checks if we detected at least one plane. */
-  private boolean hasTrackingPlane() {
-    for (Plane plane : session.getAllTrackables(Plane.class)) {
-      if (plane.getTrackingState() == TrackingState.TRACKING) {
-        return true;
-      }
-    }
-    return false;
   }
 }
